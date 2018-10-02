a39666132a635ea1a9d1ebbaf7d0202ae2eba71e
Rosy Lee <rosyhlee@gmail.com>
2018-10-02 01:03:57 +0000

    program now takes command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,8 @@ var request = require('request');
 var auth = require('./secrets.js')
 var fs = require('fs');
 
-var avatarURL = "";
-var avatarFolderFilePath = "";
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -25,13 +25,17 @@ function downloadImageByURL(url, filePath) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
   // console.log("Errors:", err);
 
-  for (var i = 0; i < result.length; i++) {
-    var avatarURL = result[i].avatar_url;
-    var avatarFolderFilePath = "avatars/" + result[i].login + ".jpg";
-    downloadImageByURL(avatarURL, avatarFolderFilePath);
+  if (repoOwner) {
+    for (var i = 0; i < result.length; i++) {
+      var avatarURL = result[i].avatar_url;
+      var avatarFolderFilePath = "avatars/" + result[i].login + ".jpg";
+      downloadImageByURL(avatarURL, avatarFolderFilePath);
+    }
+  } else {
+    console.log("Please specify a repo owner and repo name!")
   }
 
 });
